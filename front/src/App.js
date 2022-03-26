38acbf9163d67a57e444ae9fa2f85308c85ee309
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2022-03-26 00:52:12 -0300

    Algumas alteracoes

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,40 @@
+import React from "react";
+import "primeicons/primeicons.css";
+import { PrimeIcons } from "primereact/api";
 import Map from "./components/map";
+import { Sidebar } from "primereact/sidebar";
+import { Button } from "primereact/button";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
-function App() {
-  return (
-    <div className="map">
-      <Map />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { visibleLeft: true };
+  }
+  render() {
+    return (
+      <div className="card">
+        <h1 style={{ textAlign: "center" }}> GeoObras</h1>
+        <Sidebar
+          style={{ backgroundColor: "white" }}
+          visible={this.state.visibleLeft}
+          onHide={() => this.setState({ visibleLeft: false })}
+          header={"sdjbsdn"}
+        >
+          <div></div>
+        </Sidebar>
+
+        <Button
+          icon={PrimeIcons.INFO}
+          onClick={() => this.setState({ visibleLeft: true })}
+          className="mr-2"
+        />
+        <Map></Map>
+      </div>
+    );
+  }
 }
 
 export default App;
