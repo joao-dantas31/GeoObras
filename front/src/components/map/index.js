3ce26ee31a33fb9694579cbc444ed929f159958b
Eniedson <eniedson.junior@ccc.ufcg.edu.br>
2022-03-25 22:25:52 -0300

    Adicionando Controlador de camadas

--- front/src/components/map/index.js
+++ front/src/components/map/index.js
@@ -1,17 +1,31 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import { observer } from "mobx-react";
 import "./style.css";
-
-const center = [-12.966901, -50.366484];
+const center = [-8.77, -70.55];
 
 const Map = observer(() => {
   return (
     <div id="map" style={{ width: "100%", height: "100%" }}>
-      <MapContainer center={center} zoom={4} preferCanvas={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <MapContainer center={center} zoom={7} preferCanvas={true}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OpenStreetMap">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.Overlay checked name="Marker with popup">
+            <Marker position={center}>
+              <Popup>Exemplo</Popup>
+            </Marker>
+          </LayersControl.Overlay>
+        </LayersControl>
       </MapContainer>
     </div>
   );
